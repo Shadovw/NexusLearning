452dd0c42bbf8c38ac6aa0550e94dec47b735877
Shadovw <adityabss47@gmail.com>
2023-04-22 18:36:02 +0530

    visual changes

--- nexus.sql
+++ nexus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2022 at 04:59 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.28
+-- Generation Time: Apr 22, 2023 at 12:08 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `college-space`
+-- Database: `nexus`
 --
 
 -- --------------------------------------------------------
@@ -34,14 +34,7 @@ CREATE TABLE `assignment` (
   `duedate` datetime NOT NULL,
   `creationdate` datetime NOT NULL DEFAULT current_timestamp(),
   `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `assignment`
---
-
-INSERT INTO `assignment` (`assignment-name`, `assignment-desc`, `teamname`, `duedate`, `creationdate`, `id`) VALUES
-('Tutorial 1', 'LPP', 'Maths', '2022-04-15 20:14:00', '2022-04-12 20:15:03', 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,7 +48,7 @@ CREATE TABLE `assignment-upload` (
   `assign-name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `filename` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,14 +62,7 @@ CREATE TABLE `meeting` (
   `date` datetime NOT NULL,
   `title` varchar(50) NOT NULL,
   `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `meeting`
---
-
-INSERT INTO `meeting` (`teamname`, `uqid`, `date`, `title`, `id`) VALUES
-('Maths', 'd5ad743f-28b9-41f1-81b8-c1d285503d0e', '2022-04-13 20:16:00', 'Maths LPP', 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -89,14 +75,7 @@ CREATE TABLE `notes` (
   `fileaddress` varchar(200) NOT NULL,
   `id` int(11) NOT NULL,
   `filename` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`teamname`, `fileaddress`, `id`, `filename`) VALUES
-('Maths', 'C:UserskhushDesktop\nawabcollege-spacepublicuploads/2003013 Ex1.pdf', 1, '2003013 Ex1.pdf');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -109,17 +88,7 @@ CREATE TABLE `notices` (
   `email` varchar(50) NOT NULL,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
   `msg` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `notices`
---
-
-INSERT INTO `notices` (`id`, `email`, `date`, `msg`) VALUES
-(1, 'tr@g', '2022-04-04 17:24:51', 'Tommorow is Holiday'),
-(2, 'tr@g', '2022-04-04 17:25:41', 'PHP code is usually processed on a web server by a PHP interpreter implemented as a module, a daemon'),
-(3, 'tr@g', '2022-04-04 17:26:27', 'PHP code is usually processed on a web server by a PHP interpreter implemented as a module, a daemon or as a Common Gateway Interface (CGI) executable. On a web server, the result of the interpreted and executed PHP code – which may be any type of data, such as generated HTML or binary image data – would form the whole or part of an HTTP response. Various web template systems, web content management systems, and web frameworks exist which can be employed to orchestrate or facilitate the generati'),
-(4, 'tr@g', '2022-04-12 20:17:31', 'lets check notice');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -131,17 +100,16 @@ CREATE TABLE `study_on` (
   `id` int(11) NOT NULL,
   `team_name` varchar(20) NOT NULL,
   `member` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `study_on`
 --
 
 INSERT INTO `study_on` (`id`, `team_name`, `member`) VALUES
-(1, 'Maths', 's1@g'),
-(2, 'Maths', 's2@g'),
-(3, 'Maths', 's3@g'),
-(4, 'Maths', 's4@g');
+(5, 'Biology', 's1@g'),
+(6, 'Biology', 's2@g'),
+(7, 'Biology', 's3@g');
 
 -- --------------------------------------------------------
 
@@ -153,14 +121,14 @@ CREATE TABLE `teaches_on` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `team_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `teaches_on`
 --
 
 INSERT INTO `teaches_on` (`id`, `email`, `team_name`) VALUES
-(1, 'tr@g', 'Maths');
+(2, 'tr@g', 'Biology');
 
 -- --------------------------------------------------------
 
@@ -172,7 +140,7 @@ CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
   `team_name` varchar(20) NOT NULL,
   `team_description` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `teams`
@@ -192,14 +160,13 @@ CREATE TABLE `userdetail` (
   `email` varchar(30) NOT NULL,
   `password` varchar(20) NOT NULL,
   `status` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `userdetail`
 --
 
 INSERT INTO `userdetail` (`id`, `email`, `password`, `status`) VALUES
-(1, 'md.r.a.n.786@gmail.com', '123', 'student'),
 (2, 'tr@g', '123', 'teacher'),
 (3, 's1@g', '123', 'student'),
 (4, 's2@g', '123', 'student'),
@@ -273,7 +240,7 @@ ALTER TABLE `userdetail`
 -- AUTO_INCREMENT for table `assignment`
 --
 ALTER TABLE `assignment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `assignment-upload`
@@ -291,31 +258,31 @@ ALTER TABLE `meeting`
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `notices`
 --
 ALTER TABLE `notices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `study_on`
 --
 ALTER TABLE `study_on`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `teaches_on`
 --
 ALTER TABLE `teaches_on`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `userdetail`
